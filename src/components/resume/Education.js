d8f41c2293b7652fc5a8b3bf00a475eed9958c85
Syed Areeb <144684190+SyedAreeb007@users.noreply.github.com>
2025-06-04 17:03:14 +0500

    Updated Bachelors

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -37,7 +37,7 @@ const Education = () => {
           badge="2021 - 2025"
           title="Software Engineering"
           subTitle="Sukkur IBA University"
-          des="Graduating in Software Engineering from AACSB-Accredited Sukkur IBA University. Securing 3.16 CGPA."
+          des="Bachelors in Software Engineering from AACSB-Accredited & W-Category (CS Dep.) Sukkur IBA University. Securing 3.22 CGPA."
         />
         <ResumeCard
           badge="2018 - 2021"
